3babf4e874d0b724971b5a51bd10cb2af815d33c
David Dias <daviddias.p@gmail.com>
2013-07-26 23:33:05 +0100

    Job scheduler done closes #3

--- controllers/mailchimp.js
+++ controllers/mailchimp.js
@@ -1,10 +1,33 @@
 var MailChimpAPI = require('mailchimp').MailChimpAPI
   , apiKey = require('./../apikeys.js').mailchimp
 
-
+//ID OF LIST TO USE 0cd7be3684
 try { 
   var api = new MailChimpAPI(apiKey, { version : '1.3', secure : false });
 } catch (error) { console.log(error.message); }
+
+
+var createCampaign = function () {
+  var listID = '0cd7be3684'
+  var subject = 'Weekly Update'
+  var from = 'hello@bolsastecni.co'
+  var name = 'bolsastecni.co'
+  var templateID = 'Still to understand this one'
+
+  api.campaignCreate('regular'
+                    , [listID, subject, from, name] //Options
+                    , ['sdadass'] //Content
+                    , function (err, result) {
+                      if (err) {console.log(err)}
+                      console.log(result)  
+                    })
+}
+
+createCampaign()
+
+// api.lists(['list_id','list_name'], function(err, result) {
+//   console.log(result)
+// })
 
 // function subscribeUser (semail, sflname) {
 //   var user_merge_vars = {
