993471ec5533f9820dec44a10486c8c04efe0970
Nuno Nogueira <nunofmn@gmail.com>
2013-08-05 15:29:43 +0100

    Cleaned commented dummy data.

--- store/accessDB.js
+++ store/accessDB.js
@@ -13,63 +13,6 @@ db.once('open', function () {
 });
 
 
-/*
-var dummyData = {
-  scholarships :
-  [
-    {
-      "id" : "BL101", //Edital
-      "slots" : 2,
-      "type" : "introToResearch",
-      "holder" : "Prof. John",
-      "field" : "Computer Science",
-      "releaseDate" : "12-02-13",
-      "closeDate" : "17-02-13",
-      "link" : "http://example.com"
-    },
-    {
-      "id" : "BL102", //Edital
-      "slots" : 2,
-      "type" : "introToResearch",
-      "holder" : "Prof. Mario",
-      "field" : "Computer Science",
-      "releaseDate" : "12-02-13",
-      "closeDate" : "17-02-13",
-      "link" : "http://example.com"
-    },
-    {
-      "id" : "BL103", //Edital
-      "slots" : 1,
-      "type" : "introToResearch",
-      "holder" : "Prof. Maria",
-      "field" : "Computer Science",
-      "releaseDate" : "12-02-13",
-      "closeDate" : "17-02-13",
-      "link" : "http://example.com"
-    },
-    {
-      "id" : "BL104", //Edital
-      "slots" : 3,
-      "type" : "introToResearch",
-      "holder" : "Prof. Rodrigo",
-      "field" : "Management",
-      "releaseDate" : "12-02-13",
-      "closeDate" : "17-02-13",
-      "link" : "http://example.com"
-    },
-    {
-      "id" : "BL105", //Edital
-      "slots" : 1,
-      "type" : "introToResearch",
-      "holder" : "Prof. Rogalski",
-      "field" : "Quimics",
-      "releaseDate" : "12-02-13",
-      "closeDate" : "17-02-13",
-      "link" : "http://example.com"
-    }
-]};
-*/
-
 // Module methods
 var getScholarshipByID = function (id) {
   ScholarshipModel.findOne({ id: scholarship.id }, function (scholarship) {
