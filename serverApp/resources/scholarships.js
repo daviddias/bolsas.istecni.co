a4b680bc9fcd2356faa3562958339c757ac94f40
David Dias <daviddias.p@gmail.com>
2014-01-05 20:45:49 +0000

    added the route for scholarships

--- serverApp/resources/scholarships.js
+++ serverApp/resources/scholarships.js
@@ -1,3 +1,10 @@
+var Scholarship = require('./../db/models/scholarship.js');
+
 module.exports = function (request) {
-  request.reply({ greeting: 'hello world' });
+  // Se if Query String has something, if not, send all the scholarships
+  Scholarship.findAll(function (err, scholarships){
+    request.reply(scholarships);
+  });
+
+  // TODO implement the options using queryString
 }
